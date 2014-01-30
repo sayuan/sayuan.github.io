6e6e981c3be623e9e12dfb80514f659f3cb8fdf6
sayuan <shiao.an.yuan@gmail.com>
2014-01-30 13:54:57 +0800

    Add google analytics

--- conf.py
+++ conf.py
@@ -532,6 +532,13 @@ BODY_END = """
     (function () {
         cjkSpaceFix();
     })();
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-47613541-1', 'sayuan.github.io');
+  ga('send', 'pageview');
 </script>
 """
 
