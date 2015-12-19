487cb360a54f0bb65ac5e2d33d84deb967920b3b
sayuan <shiao.an.yuan@gmail.com>
2015-12-19 15:05:13 +0800

    Automatic update

--- assets/js/all-nocdn.js
+++ assets/js/all-nocdn.js
@@ -198,15 +198,3 @@ u[o]&&(delete u[o],c?delete n[l]:typeof n.removeAttribute!==i?n.removeAttribute(
   document.createElement("abbr");
   document.createElement("time");
 }));
-
-//  We wait for the onload function to load MathJax after the page is completely loaded.  
-//  MathJax is loaded 1 unit of time after the page is ready.
-//  This hack prevent problems when you use social button from addthis.
-//
-window.onload = function () {
-  setTimeout(function () {
-    var script = document.createElement("script");
-    script.src  = "https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML";
-    document.getElementsByTagName("body")[0].appendChild(script);
-  },1)
-}
