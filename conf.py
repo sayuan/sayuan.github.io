4a0a688f0d093742d2bcc33f819b71d12991fde1
sayuan <shiaoan.yuan@appier.com>
2021-02-17 13:48:32 +0800

    Update GA tracking code

--- conf.py
+++ conf.py
@@ -910,17 +910,16 @@ COMMENT_SYSTEM_ID = "say-something"
 # (translatable)
 BODY_END = """
 <script type="text/javascript" src="/assets/js/cjk_space_fix.js"></script>
-<script type="text/javascript">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-47613541-1"></script>
+<script>
   cjkSpaceFix();
 
-  // Google Analytics
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  ga('create', 'UA-47613541-1', 'sayuan.github.io');
-  ga('send', 'pageview');
+  gtag('config', 'UA-47613541-1');
 </script>
 """
 
